410f235408fd27a9b0f4d292ca4bb19d341e8b84
Ilya <fominiv10@gmail.com>
2022-09-23 21:45:50 +0300

    solved the forty seventh  task

---
+++ Task 47/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+int[,] Getarray (int n, int m)
+{
+    int[,] array = new int [n, m];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            array[i, j] = new Random().Next(-1000, 1000);
+        }
+    }
+return array;
+}
+
+void OutputDisplay (int[,] array)
+{
+     for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }        
+}
+
+
+Console.WriteLine("Введите количество строк");
+int line = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int column = int.Parse(Console.ReadLine());
+int[,] array = Getarray (line, column);
+OutputDisplay (array);
